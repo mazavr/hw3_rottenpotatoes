54016fd9d76cbe0ac57a57a759c6fbffe24a57fc
sergey <boxblast@gmail.com>
2013-02-10 22:13:22 +0400

    gemfile fix

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -22,14 +22,16 @@ Background: movies have been added to database
   And  I am on the RottenPotatoes home page
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
+  Given I check the following ratings: PG, R
+  When I press "Refresh"
+  Then I should see movies with ratings: PG, R
 
 Scenario: no ratings selected
-  # see assignment
+  Given I uncheck all ratings
+  When I press "Refresh"
+  Then I should see all of the movies
 
 Scenario: all ratings selected
-  # see assignment
+  Given I check all ratings
+  When I press "Refresh"
+  Then I should see all of the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -12,7 +12,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  page.body.index(e1).should < page.body.index(e2)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -20,7 +20,41 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  unless uncheck
+    Movie.all_ratings.each do |rating|
+      step(%{I uncheck "ratings_#{rating}"})
+    end
+  end
+  rating_list.delete(' ').split(',').each do |rating|
+    if uncheck
+      step(%{I uncheck "ratings_#{rating}"})
+    else
+      step(%{I check "ratings_#{rating}"})
+    end
+  end
+end
+
+Then /^I should see all of the movies/ do
+  rows = page.all("table#movies tbody tr td[1]").map! {|t| t.text}
+  assert ( rows.size == Movie.all.count )
+end
+
+Then /^I should see movies with ratings: (.*)/ do |rating_list|
+  ratings = rating_list.delete(' ').split(',')
+  page.all('table#movies tbody tr td[2]').each do |td|
+    movie_rating = td.text.delete("\n").delete("\r").strip()
+    ratings.should include movie_rating
+  end
+end
+
+Given /^I uncheck all ratings/ do
+  Movie.all_ratings.each do |rating|
+    step(%{I uncheck "ratings_#{rating}"})
+  end
+end
+
+Given /^I check all ratings/ do
+  Movie.all_ratings.each do |rating|
+    step(%{I check "ratings_#{rating}"})
+  end
 end
